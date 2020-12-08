46ba09a9b9fe80cbf4f9f48cf34e1856b3205f98
adamcameron <cameron.adam@gmail.com>
2020-12-08 13:50:57 +0000

    PHP working. Possibly needs tidy-up

---
+++ public/gdayWorld.php
@@ -0,0 +1,4 @@
+<?php
+
+$message = "G'day World";
+echo $message;
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "URL requested: " . $_SERVER['SCRIPT_NAME'] . '<br>';
+echo "Path requested: " . $_SERVER['REQUEST_URI'] . '<br>';
