da50382b2d39fad933adb790d439ef490394fedc
adamcameron <cameron.adam@gmail.com>
2021-01-21 10:03:54 +0000

    GithubProfile revised for blog article

--- public/assets/scripts/githubProfiles.js
+++ public/assets/scripts/githubProfiles.js
@@ -1,19 +1,50 @@
 let githubUserCardComponent = {
     template : "#github-user-card-template",
+    props : {
+        username: {
+            type: String,
+            required: true
+        }
+    },
     data : function () {
         return {
-            name : "Kristy",
-            pageUrl : "http://webserver.backend/GITHUB_PAGE_URL",
-            avatar : "https://semantic-ui.com/images/avatar2/large/kristy.png",
-            joinedYear : "2013_BREAK_ME",
-            description : "Kristy is an art director living in New York.",
-            friends : 22,
-            friendsPageUrl: "http://webserver.backend/GITHUB_FRIENDS_PAGE_URL"
+            name : null,
+            pageUrl : null,
+            avatar : null,
+            joinedYear : null,
+            description : null,
+            friends : null,
+            friendsPageUrl: null
         };
+    },
+    created () {
+        axios.get(
+            `https://api.github.com/users/${this.username}`,
+            {
+                auth: {
+                    username: this.$route.query.GITHUB_PERSONAL_ACCESS_TOKEN
+                }
+            }
+        )
+        .then(response => {
+            this.name = response.data.name;
+            this.pageUrl = response.data.html_url;
+            this.avatar = response.data.avatar_url;
+            this.joinedYear = new Date(response.data.created_at).getFullYear();
+            this.description = response.data.bio;
+            this.friends = response.data.followers;
+            this.friendsPageUrl = response.data.html_url + "?tab=followers";
+        });
     }
 };
 
+let router = new VueRouter({
+    mode: 'history',
+    routes: []
+});
+
 new Vue({
+    router,
     el: '#app',
     components: {
         "github-user-card" : githubUserCardComponent
