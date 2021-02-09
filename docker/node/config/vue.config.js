3133924f6420f9c874cb0a6a4e2ea7c174d66f2c
adamcameron <cameron.adam@gmail.com>
2021-02-09 12:59:05 +0000

    a few tweaks to fix vue, remove stuff I don't need, move the log files

--- docker/node/config/vue.config.js
+++ docker/node/config/vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
     devServer: {
         host: "vuejs.backend",
         disableHostCheck: false,
-        port: 8080
+        port: 8080,
+        watchOptions : {
+            ignored: /node_modules/,
+            poll: 1000
+        }
     }
 }
